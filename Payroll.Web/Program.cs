5899f36f68f313bd19a3e4926841340829bcbaf1
Eduardo Tamayo <tamayo.eduardo@hotmail.com>
2026-03-11 19:27:18 -0300

    Config Api + DB

--- Payroll.Web/Program.cs
+++ Payroll.Web/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // Usa una sola instancia limpia
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://payroll-api-server.azurewebsites.net/")
+     BaseAddress = new Uri("https://payroll-api-servicios.azurewebsites.net/")
 });
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
